981323bc927691b50ba83a9e7ddb4949a72b5d57
richardykedang <kedangrichard@gmail.com>
2021-01-24 21:36:10 +0700

    modify task all view

--- src/views/task/TaskAll.vue
+++ src/views/task/TaskAll.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1> Task All Route</h1>
+    <div class="d-flex flex-column">
+        <h1>Tasks</h1>
+
+        <div class="mb-4">
+            <router-link to="/tasks/new" class="btn btn-success ml-2" exact>Create Task</router-link>
+        </div>
+    </div>
 </template>
